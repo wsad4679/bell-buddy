92f0169ca910cae26f386611f01b85eb69d09057
SzymonH <szymonh4679@gmail.com>
2025-06-05 19:16:52 +0200

    add choice of diffrent time zone

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import CountDownTimer from "./CountDownTimer.tsx";
 
 
 const Clock = () => {
@@ -6,12 +7,22 @@ const Clock = () => {
 
     const [time, setTime] = useState<string>("");
     const [showTimer, setShowTimer] = useState<boolean>(false);
+    const [showCountDown, setShowCountDown] = useState<boolean>(false);
+    const [selectedZone, setSelectedZone] = useState<string>("Europe/Warsaw");
+
+    const timeZones = [
+        "Europe/Warsaw",
+        "UTC",
+        "America/New_York",
+        "Asia/Tokyo",
+        "Australia/Sydney"
+    ];
 
     useEffect(() => {
 
         const updateTime =() => {
             const now = new Date();
-            const formattedTime = now.toLocaleTimeString();
+            const formattedTime = now.toLocaleTimeString("pl-PL",{timeZone: selectedZone});
             setTime(formattedTime);
         }
 
@@ -21,7 +32,7 @@ const Clock = () => {
 
         return() => clearInterval(interval);
 
-    }, [])
+    }, [selectedZone]);
 
 
     return (
@@ -29,6 +40,21 @@ const Clock = () => {
             {
                 showTimer && <div>
                     Aktualny czas: {time}
+
+                    <label> Wybierz strefę czasową: </label>
+
+
+                    <select
+                        value={selectedZone}
+                        onChange={(e) => setSelectedZone(e.target.value)}
+                        style={{ margin: "10px", padding: "5px" }}
+                    >
+                        {timeZones.map((zone) => (
+                            <option key={zone} value={zone}>
+                                {zone}
+                            </option>
+                        ))}
+                    </select>
                 </div>
             }
 
@@ -42,6 +68,19 @@ const Clock = () => {
             >Schowaj zegarek</button>
 
 
+
+            {
+                showCountDown && <CountDownTimer/>
+            }
+
+            <button onClick={() => setShowCountDown(true)}
+                    style={{color:"beige", backgroundColor: "#123abc", margin:"10px",
+                        padding:"20px"}}
+            >Pokaż Licznik</button>
+            <button onClick={() => setShowCountDown(false)}
+                    style={{color:"#123abc", backgroundColor: "beige", margin:"10px",
+                        padding:"20px"}}
+            >Schowaj Licznik</button>
 
 
         </>
