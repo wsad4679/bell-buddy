f1d7856f19f0285c9232f653f0829d8b0e337466
SzymonH <szymonh4679@gmail.com>
2025-06-05 17:33:26 +0200

    create form

--- src/Components/AddStudentForm.tsx
+++ src/Components/AddStudentForm.tsx
@@ -1,10 +1,39 @@
+import React, {useState} from "react";
+
 const AddStudentForm = () =>
 {
+    interface Student {
+        id: number;
+        name: string;
+        surname: string;
+        isPresent: boolean;
+    }
 
-    
+    const [students, setStudents] = useState<Student>({
+        id: 0,
+        name: "",
+        surname: "",
+        isPresent: false,
+    });
+
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const {name, value, type, checked} = event.target as HTMLInputElement;
+        setStudents(prevState => ({...prevState, [name]: type === "checkbox" ? checked : value}));
+    }
+
+
+    function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
+
+    }
 
     return(
-        <h2>student form</h2>
+        <form onSubmit={handleSubmit}>
+        <h3>Student Form</h3>
+        Imie: <input type="text" onChange={handleChange } value={students.name} name="name"/>
+        Nazwisko: <input type="text" onChange={handleChange} value={students.surname} name="surname"/>
+        Czy obecny: <input type="checkbox" onChange={handleChange} checked={students.isPresent} name="isPresent"/>
+        </form>
     )
 }
 
